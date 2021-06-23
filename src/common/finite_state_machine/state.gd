45313554003dde93679b4c3753139387a400fa8b
Martin Gulliksson <martin@gullik.cc>
2021-06-23 20:14:38 +0200

    add finite state machine code

---
+++ src/common/finite_state_machine/state.gd
@@ -0,0 +1,37 @@
+# https://www.gdquest.com/tutorial/godot/design-patterns/finite-state-machine
+# Virtual base class for all states.
+class_name State
+extends Node
+
+
+# Virtual function. Receives events from the `_unhandled_input()` callback.
+func unhandled_input(_event: InputEvent) -> void:
+	pass
+
+
+# Virtual function. Corresponds to the `_process()` callback.
+func process(_delta: float) -> void:
+	pass
+
+
+# Virtual function. Corresponds to the `_physics_process()` callback.
+func physics_process(_delta: float) -> void:
+	pass
+
+
+# Virtual function. Called by the state machine to determine if the state should be changed. Returns
+# an empty string if the state should not be changed.
+func get_transition() -> String:
+	return ""
+
+
+# Virtual function. Called by the state machine upon changing the active state. The `data` parameter
+# is a dictionary with arbitrary data the state can use to initialize itself.
+func enter(_data := {}) -> void:
+	pass
+
+
+# Virtual function. Called by the state machine before changing the active state. Use this function
+# to clean up the state.
+func exit() -> void:
+	pass
---
+++ src/common/finite_state_machine/state_machine.gd
@@ -0,0 +1,47 @@
+# https://www.gdquest.com/tutorial/godot/design-patterns/finite-state-machine
+# Generic state machine. Initializes states and delegates engine callbacks
+# (_process, _physics_process, _unhandled_input) to the active state.
+class_name StateMachine
+extends Node
+
+# Path to the initial active state. We export it to be able to pick the initial state in the inspector.
+@export var initial_state := NodePath()
+
+# The current active state. At the start of the game, we get the `initial_state`.
+@onready var state: State = get_node(initial_state)
+
+
+# Enter the initial state when initializing the state machine.
+func _ready() -> void:
+	assert(state != null)
+	state.enter()
+
+
+# Delegate `_unhandled_input` callback to the active state.
+func _unhandled_input(event: InputEvent) -> void:
+	state.unhandled_input(event)
+
+
+# Delegate `_process` callback to the active state.
+func _process(delta: float) -> void:
+	state.process(delta)
+
+
+# Delegate `_physics_process` callback to the active state.
+func _physics_process(delta: float) -> void:
+	state.physics_process(delta)
+
+	# Change the current state
+	var target_state_name: String = state.get_transition()
+	if not target_state_name.is_empty():
+		call_deferred(transition_to.get_method(), target_state_name)
+
+
+# This function calls the current state's exit() function, then changes the active state,
+# and calls its enter function.
+# It optionally takes a `data` dictionary to pass to the next state's enter() function.
+func transition_to(target_state_name: String, data: Dictionary = {}) -> void:
+	assert(has_node(target_state_name))
+	state.exit()
+	state = get_node(target_state_name)
+	state.enter(data)
