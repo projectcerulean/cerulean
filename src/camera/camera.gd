6e1cff791801d9940a5b3d56e513648f676620b4
Martin Gulliksson <martin@gullik.cc>
2021-06-27 23:48:48 +0200

    camera: controls for zooming in and out

--- src/camera/camera.gd
+++ src/camera/camera.gd
@@ -10,6 +10,8 @@ extends Camera3D
 @export var pitch_limit: Vector2 = Vector2(-89, 89)
 @export var anchor_offset: Vector3
 @export var look_target: Vector3
+@export var zoom_limit: Vector2 = Vector2(2.5, 25.0)
+@export var zoom_speed: float = 2.0
 
 @onready var target: Node3D = get_node(target_path) as Node3D
 @onready var thumbstick_right: Thumbstick = get_node(thumbstick_right_path)
@@ -23,14 +25,22 @@ func _ready() -> void:
 	assert(target != null)
 	if target_offset == Vector3.ZERO:
 		target_offset = transform.origin - target.transform.origin - anchor_offset
+	assert(clamp(target_offset.length(), zoom_limit.x, zoom_limit.y) == target_offset.length())
 	if look_target == Vector3.ZERO:
 		look_target = Vector3(0, 0, -100)
 
 
 func _process(delta: float) -> void:
-	camera_rotation_rad.x = camera_rotation_rad.x - thumbstick_right.value.y * rotation_speed_rad.x * delta
-	camera_rotation_rad.x = clamp(camera_rotation_rad.x, pitch_limit_rad.x, pitch_limit_rad.y)
-	camera_rotation_rad.y = camera_rotation_rad.y - thumbstick_right.value.x * rotation_speed_rad.y * delta
+	if Input.is_action_pressed("camera_move_zoom_toggle"):
+		target_offset *= 1.0 + (thumbstick_right.value.y) * zoom_speed * delta
+		if target_offset.length() < zoom_limit.x:
+			target_offset = target_offset.normalized() * zoom_limit.x
+		elif target_offset.length() > zoom_limit.y:
+			target_offset = target_offset.normalized() * zoom_limit.y
+	else:
+		camera_rotation_rad.x = camera_rotation_rad.x - thumbstick_right.value.y * rotation_speed_rad.x * delta
+		camera_rotation_rad.x = clamp(camera_rotation_rad.x, pitch_limit_rad.x, pitch_limit_rad.y)
+		camera_rotation_rad.y = camera_rotation_rad.y - thumbstick_right.value.x * rotation_speed_rad.y * delta
 
 	transform.origin = target.transform.origin + anchor_offset
 	var vertical_axis: Vector3 = Vector3.RIGHT.rotated(Vector3.UP, camera_rotation_rad.y)
