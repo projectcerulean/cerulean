5499d0b6919f1ce6d59364fd6a4d5f9528759d42
Martin Gulliksson <martin@gullik.cc>
2021-06-27 22:47:13 +0200

    camera: start writing new camera controller

---
+++ src/camera/camera.gd
@@ -0,0 +1,43 @@
+# Reference: https://www.youtube.com/watch?v=vEPpS120J7A
+extends Camera3D
+
+@export var target_path: NodePath
+@export var thumbstick_right_path := NodePath()
+
+@export var rotation_speed: Vector2 = Vector2(90, 180)
+@export var target_offset: Vector3
+@export var camera_rotation: Vector3
+@export var pitch_limit: Vector2 = Vector2(-89, 89)
+@export var anchor_offset: Vector3
+@export var look_target: Vector3
+
+@onready var target: Node3D = get_node(target_path) as Node3D
+@onready var thumbstick_right: Thumbstick = get_node(thumbstick_right_path)
+
+@onready var rotation_speed_rad: Vector2 = Vector2(deg2rad(rotation_speed.x), deg2rad(rotation_speed.y))
+@onready var camera_rotation_rad: Vector2 = Vector2(deg2rad(camera_rotation.x), deg2rad(camera_rotation.y))
+@onready var pitch_limit_rad: Vector2 = Vector2(deg2rad(pitch_limit.x), deg2rad(pitch_limit.y))
+
+
+func _ready() -> void:
+	assert(target != null)
+	if target_offset == Vector3.ZERO:
+		target_offset = transform.origin - target.transform.origin - anchor_offset
+	if look_target == Vector3.ZERO:
+		look_target = Vector3(0, 0, -100)
+
+
+func _process(delta: float) -> void:
+	camera_rotation_rad.x = camera_rotation_rad.x - thumbstick_right.value.y * rotation_speed_rad.x * delta
+	camera_rotation_rad.x = clamp(camera_rotation_rad.x, pitch_limit_rad.x, pitch_limit_rad.y)
+	camera_rotation_rad.y = camera_rotation_rad.y - thumbstick_right.value.x * rotation_speed_rad.y * delta
+
+	transform.origin = target.transform.origin + anchor_offset
+	var vertical_axis: Vector3 = Vector3.RIGHT.rotated(Vector3.UP, camera_rotation_rad.y)
+	var target_offset_rotated: Vector3 = target_offset.rotated(Vector3.UP, camera_rotation_rad.y)
+	var look_target_rotated: Vector3 = look_target.rotated(Vector3.UP, camera_rotation_rad.y)
+	target_offset_rotated = target_offset_rotated.rotated(vertical_axis, camera_rotation_rad.x)
+	look_target_rotated = look_target_rotated.rotated(vertical_axis, camera_rotation_rad.x)
+
+	transform.origin += target_offset_rotated
+	look_at(look_target_rotated + target.transform.origin)
--- src/entities/player/player.gd
+++ src/entities/player/player.gd
@@ -2,9 +2,8 @@ class_name Player
 extends CharacterBody3D
 
 @export var camera_path := NodePath()
-@export var camera_pivot_path := NodePath()
 @export var thumbstick_left_path := NodePath()
-@export var thumbstick_right_path := NodePath()
+
 @export var move_acceleration: float = 150.0
 @export var move_friction_coefficient: float = 15.0
 @export var move_acceleration_air: float = move_acceleration / 7.0
@@ -12,22 +11,13 @@ extends CharacterBody3D
 @export var jump_speed: float = 10.0
 @export var jump_acceleration: float = 5.0
 @export var gravity: float = 20.0
-@export var camera_move_speed: float = PI
 
 @onready var camera: Camera3D = get_node(camera_path)
-@onready var camera_pivot: Position3D = get_node(camera_pivot_path)
 @onready var thumbstick_left: Thumbstick = get_node(thumbstick_left_path)
-@onready var thumbstick_right: Thumbstick = get_node(thumbstick_right_path)
 @onready var raycast: RayCast3D = get_node("RayCast3D")
 
 
 func _ready() -> void:
 	assert(camera != null)
-	assert(camera_pivot != null)
 	assert(thumbstick_left != null)
-	assert(thumbstick_right != null)
 	assert(raycast != null)
-
-
-func _process(delta) -> void:
-	camera_pivot.rotate(Vector3.UP, -thumbstick_right.value.x * camera_move_speed * delta)
