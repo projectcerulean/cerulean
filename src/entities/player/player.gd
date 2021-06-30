36f2d4e86907df235d3c7a8576f59cc003828604
Martin Gulliksson <martin@gullik.cc>
2021-06-30 22:22:37 +0200

    camera, player: smooth camera y

--- src/entities/player/player.gd
+++ src/entities/player/player.gd
@@ -11,8 +11,13 @@ extends CharacterBody3D
 @export var jump_acceleration: float = 10.0
 @export var air_control_modifier: float = 0.08
 
+@export var camera_anchor_y_smooth_grounded: float = 0.1
+@export var camera_anchor_y_smooth_air: float = 0.08
+
 @onready var camera: Camera3D = get_node(camera_path)
 @onready var thumbstick_left: Thumbstick = get_node(thumbstick_left_path)
+@onready var camera_anchor: Position3D = get_node("CameraAnchor")
+@onready var state_machine: StateMachine = get_node("StateMachine")
 @onready var raycast: RayCast3D = get_node("RayCast3D")
 @onready var coyote_timer: Timer = get_node("CoyoteTimer")
 @onready var jump_buffer_timer: Timer = get_node("JumpBufferTimer")
@@ -22,8 +27,10 @@ var direction: Vector3 = Vector3.FORWARD
 var move_acceleration_air: float = move_acceleration * air_control_modifier
 var move_friction_coefficient_air: float = move_friction_coefficient * air_control_modifier
 
+
 func _ready() -> void:
 	assert(camera != null)
+	assert(camera_anchor != null)
 	assert(thumbstick_left != null)
 	assert(raycast != null)
 	assert(coyote_timer != null)
@@ -34,3 +41,12 @@ func _ready() -> void:
 func _process(_delta) -> void:
 	assert(direction.y == 0)
 	mesh_instance.look_at(mesh_instance.get_global_transform().origin + direction)
+
+	# The camera anchor is a Position3D which follows a bit after the player for a smoother feel.
+	# It has a top level transform, i.e. its position is not directly inherited from the player.
+	camera_anchor.position.x = position.x
+	camera_anchor.position.z = position.z
+	if state_machine.state.name == "Run" or state_machine.state.name == "Idle":
+		camera_anchor.position.y = lerp(camera_anchor.position.y, position.y, camera_anchor_y_smooth_grounded)
+	else:
+		camera_anchor.position.y = lerp(camera_anchor.position.y, position.y, camera_anchor_y_smooth_air)
