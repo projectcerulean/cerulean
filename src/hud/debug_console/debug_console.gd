f4d355f581a2b10a380e6337e7e48f7afff7ca00
Martin Gulliksson <martin@gullik.cc>
2021-07-02 12:36:20 +0200

    debug_console: add limit on buffer size

--- src/hud/debug_console/debug_console.gd
+++ src/hud/debug_console/debug_console.gd
@@ -1,15 +1,27 @@
 extends TextEdit
 
+@export var buffer_size_lines: int = 25
+
+var contents: Array[String]
+var index_current: int = 0
+var n_lines_current: int = 0
+
 
 func _ready():
 	Signals.connect(Signals.debug_write.get_name(), self._on_debug_write)
-	text = ""
+	contents.resize(buffer_size_lines)
 
 
 func _on_debug_write(sender: Node, string: String):
 	visible = true
-	if text.length() > 0:
-		text = "%s\n[%s]: %s" % [text, sender.name, string]
+	contents[index_current] = "[%s]: %s" % [sender.name, string]
+	index_current = (index_current + 1) % buffer_size_lines
+	if n_lines_current == buffer_size_lines:
+		if index_current == 0:
+			text = "\n".join(contents.slice(index_current, buffer_size_lines - 1))
+		else:
+			text = "\n".join(contents.slice(index_current, buffer_size_lines - 1) + contents.slice(0, index_current - 1))
 	else:
-		text = "[%s]: %s" % [sender.name, string]
+		text = "\n".join(contents.slice(0, n_lines_current))
+		n_lines_current += 1
 	set_v_scroll(INF)
