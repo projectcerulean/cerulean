879712c6a550f92d5aab5c09b3de8eb478341d1f
Martin Gulliksson <martin@gullik.cc>
2021-07-02 23:42:33 +0200

    debug_console: colored node names

--- src/hud/debug/debug_console/debug_console.gd
+++ src/hud/debug/debug_console/debug_console.gd
@@ -1,21 +1,23 @@
-extends TextEdit
+extends ColorRect
 
 @export var buffer_size_lines: int = 25
 
+@onready var richTextLabel: RichTextLabel = get_node("RichTextLabel")
 @onready var rotationQueue: DataStructures.RotationQueue = DataStructures.RotationQueue.new(buffer_size_lines)
 
 
 func _ready():
 	Signals.connect(Signals.debug_write.get_name(), self._on_debug_write)
+	assert(richTextLabel != null)
 	assert(rotationQueue != null)
 
 
 func _on_debug_write(sender: Node, string: String):
-	visible = true
-	rotationQueue.add("[%s]: %s" % [sender.name, string])
+	var hexColor: String = CColor.str_to_color(sender.name).to_html(false)
+	rotationQueue.add("[color=#%s][code][%s]:[/code][/color] %s" % [hexColor, sender.name, string])
 	var lines: Array[String]
 	lines.resize(rotationQueue.size())
 	for iLine in range(rotationQueue.size()):
 		lines[iLine] = rotationQueue.get_item(iLine)
-	text = "\n".join(lines)
-	set_v_scroll(INF)
+	richTextLabel.bbcode_text = "\n".join(lines)
+	visible = true
