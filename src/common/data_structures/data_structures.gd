b203d884eb4df6940bf3411ec90b123ef7d9e80b
Martin Gulliksson <martin@gullik.cc>
2021-07-02 22:37:54 +0200

    data_structures: add rotation queue class

---
+++ src/common/data_structures/data_structures.gd
@@ -0,0 +1,32 @@
+class_name DataStructures
+extends Node
+
+
+class RotationQueue:
+	var contents: Array[Variant]
+	var index_current: int = 0
+	var size_current: int = 0
+
+
+	func _init(size: int):
+		assert(size > 0)
+		contents.resize(size)
+
+
+	func add(element: Variant) -> void:
+		contents[index_current] = element
+		index_current = (index_current + 1) % contents.size()
+		size_current = min(size_current + 1, contents.size())
+
+
+	func get_item(index: int) -> Variant:
+		assert(index >= 0 and index < size_current)
+		return contents[(index_current + index) % size_current]
+
+
+	func size() -> int:
+		return size_current
+
+
+	func size_max() -> int:
+		return contents.size()
--- src/hud/debug/debug_console/debug_console.gd
+++ src/hud/debug/debug_console/debug_console.gd
@@ -2,26 +2,20 @@ extends TextEdit
 
 @export var buffer_size_lines: int = 25
 
-var contents: Array[String]
-var index_current: int = 0
-var n_lines_current: int = 0
+@onready var rotationQueue: DataStructures.RotationQueue = DataStructures.RotationQueue.new(buffer_size_lines)
 
 
 func _ready():
 	Signals.connect(Signals.debug_write.get_name(), self._on_debug_write)
-	contents.resize(buffer_size_lines)
+	assert(rotationQueue != null)
 
 
 func _on_debug_write(sender: Node, string: String):
 	visible = true
-	contents[index_current] = "[%s]: %s" % [sender.name, string]
-	index_current = (index_current + 1) % buffer_size_lines
-	if n_lines_current == buffer_size_lines:
-		if index_current == 0:
-			text = "\n".join(contents.slice(index_current, buffer_size_lines - 1))
-		else:
-			text = "\n".join(contents.slice(index_current, buffer_size_lines - 1) + contents.slice(0, index_current - 1))
-	else:
-		text = "\n".join(contents.slice(0, n_lines_current))
-		n_lines_current += 1
+	rotationQueue.add("[%s]: %s" % [sender.name, string])
+	var lines: Array[String]
+	lines.resize(rotationQueue.size())
+	for iLine in range(rotationQueue.size()):
+		lines[iLine] = rotationQueue.get_item(iLine)
+	text = "\n".join(lines)
 	set_v_scroll(INF)
