f2cb65f49cc91915093a2ba03cce5a1ec5b4fd51
Martin Gulliksson <martin@gullik.cc>
2021-07-02 21:23:30 +0200

    ccolor: color utility class

--- src/hud/debug/vector_visualizer/vector_visualizer.gd
+++ src/hud/debug/vector_visualizer/vector_visualizer.gd
@@ -26,8 +26,5 @@ func _on_visualize_vector(sender: Node, vector: Vector2):
 	visible = true
 	vectors[sender] = vector
 	if not colors.has(sender):
-		var color_r: float = (str(sender.name).sha256_text().substr(0, 8).hex_to_int() % 255) / 255.0
-		var color_g: float = (str(color_r).sha256_text().substr(0, 8).hex_to_int() % 255) / 255.0
-		var color_b: float = (str(color_g).sha256_text().substr(0, 8).hex_to_int() % 255) / 255.0
-		colors[sender] = Color(color_r, color_g, color_b).lerp(Color.WHITE, 0.5)
+		colors[sender] = CColor.str_to_color(sender.name)
 	update()
