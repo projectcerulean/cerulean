9499be2b8912bea22aea79936d40b2bf2924b066
Martin Gulliksson <martin@gullik.cc>
2021-07-03 22:24:16 +0200

    player: render glide trail lines using immediate mesh

---
+++ src/entities/player/player_glide_trail_renderer.gd
@@ -0,0 +1,60 @@
+extends MeshInstance3D
+
+@export var trail_position_left_path: NodePath
+@export var trail_position_right_path: NodePath
+@export var max_points: int = 50
+
+@onready var trail_position_left = get_node(trail_position_left_path)
+@onready var trail_position_right = get_node(trail_position_right_path)
+
+var gliding: bool = false
+
+var pointQueueLeft: DataStructures.RotationQueue = DataStructures.RotationQueue.new(max_points)
+var pointQueueRight: DataStructures.RotationQueue = DataStructures.RotationQueue.new(max_points)
+
+
+func _ready():
+	Signals.connect(Signals.state_entered.get_name(), self._on_state_entered)
+	Signals.connect(Signals.state_exited.get_name(), self._on_state_exited)
+	assert(trail_position_left != null)
+	assert(trail_position_right != null)
+
+
+func _process(delta: float) -> void:
+	if gliding:
+		pointQueueLeft.add(trail_position_left.global_transform.origin - global_transform.origin)
+		pointQueueRight.add(trail_position_right.global_transform.origin - global_transform.origin)
+	else:
+		pointQueueLeft.add(Vector3.ZERO)
+		pointQueueRight.add(Vector3.ZERO)
+
+	var pointsToDraw: Array[Vector3]
+	for iPoint in range(0, pointQueueLeft.size() - 1):
+		var p1: Vector3 = pointQueueLeft.get_item(iPoint)
+		var p2: Vector3 = pointQueueLeft.get_item(iPoint + 1)
+		if p1 != Vector3.ZERO and p2 != Vector3.ZERO:
+			pointsToDraw.append(p1)
+			pointsToDraw.append(p2)
+	for iPoint in range(0, pointQueueRight.size() - 1):
+		var p1: Vector3 = pointQueueRight.get_item(iPoint)
+		var p2: Vector3 = pointQueueRight.get_item(iPoint + 1)
+		if p1 != Vector3.ZERO and p2 != Vector3.ZERO:
+			pointsToDraw.append(p1)
+			pointsToDraw.append(p2)
+
+	mesh.clear_surfaces()
+	if pointsToDraw.size() > 0:
+		mesh.surface_begin(Mesh.PRIMITIVE_LINES)
+		for point in pointsToDraw:
+			mesh.surface_add_vertex(point)
+		mesh.surface_end()
+
+
+func _on_state_entered(sender: Node, state_name: String) -> void:
+	if sender.owner == owner and state_name == "Glide":
+		gliding = true
+
+
+func _on_state_exited(sender: Node, state_name: String) -> void:
+	if sender.owner == owner and state_name == "Glide":
+		gliding = false
