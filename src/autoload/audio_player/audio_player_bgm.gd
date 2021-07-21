bd64375cce806682143f85ca46f16f8c03e24be0
Martin Gulliksson <martin@gullik.cc>
2021-07-21 21:29:47 +0200

    audio_player: mute tracks completely after fading out

--- src/autoload/audio_player/audio_player_bgm.gd
+++ src/autoload/audio_player/audio_player_bgm.gd
@@ -1,11 +1,14 @@
 extends Node
 
+const volume_db_zero: float = -80.0
+
 @export var player_path := NodePath()
 
 @export var volume_db_low: float = -30.0
 @export var volume_db_high: float = 0.0
 
-@export var tween_duration: float = 3.0
+@export var tween_duration_glide: float = 3.0
+@export var tween_duration_cutoff: float = 0.01
 
 @onready var base_player: AudioStreamPlayer = get_node("BasePlayer")
 @onready var glide_player: AudioStreamPlayer = get_node("GlidePlayer")
@@ -23,9 +26,9 @@ func _ready() -> void:
 	assert(glide_player != null)
 	assert(rhythm_player != null)
 
-	base_player.volume_db = volume_db_low
-	glide_player.volume_db = volume_db_low
-	rhythm_player.volume_db = volume_db_low
+	base_player.volume_db = volume_db_zero
+	glide_player.volume_db = volume_db_zero
+	rhythm_player.volume_db = volume_db_zero
 
 	base_player.stream = load("res://assets/sound/bgm/seaandsky/seaandsky.wav")
 	glide_player.stream = load("res://assets/sound/bgm/seaandsky/seaandsky_glide.wav")
@@ -50,7 +53,8 @@ func _ready() -> void:
 	if base_tween != null:
 		base_tween.kill()
 	base_tween = create_tween()
-	base_tween.tween_property(base_player, "volume_db", volume_db_high, tween_duration)
+	base_tween.tween_property(base_player, "volume_db", volume_db_low, tween_duration_cutoff)
+	base_tween.tween_property(base_player, "volume_db", volume_db_high, tween_duration_glide)
 
 
 func _on_state_entered(sender: Node, state_name: String) -> void:
@@ -60,15 +64,19 @@ func _on_state_entered(sender: Node, state_name: String) -> void:
 			glide_tween.kill()
 		glide_tween = create_tween()
 		if state_name == player.GLIDE:
-			glide_tween.tween_property(glide_player, "volume_db", volume_db_high, tween_duration)
+			glide_tween.tween_property(glide_player, "volume_db", volume_db_low, tween_duration_cutoff)
+			glide_tween.tween_property(glide_player, "volume_db", volume_db_high, tween_duration_glide)
 		else:
-			glide_tween.tween_property(glide_player, "volume_db", volume_db_low, tween_duration)
+			glide_tween.tween_property(glide_player, "volume_db", volume_db_low, tween_duration_glide)
+			glide_tween.tween_property(glide_player, "volume_db", volume_db_zero, tween_duration_cutoff)
 
 		# TODO: control the rhythm layer player depending on the amount of action on-screen
 		if rhythm_tween != null:
 			rhythm_tween.kill()
 		rhythm_tween = create_tween()
 		if state_name == player.GLIDE:
-			rhythm_tween.tween_property(rhythm_player, "volume_db", volume_db_high, tween_duration)
+			rhythm_tween.tween_property(rhythm_player, "volume_db", volume_db_low, tween_duration_cutoff)
+			rhythm_tween.tween_property(rhythm_player, "volume_db", volume_db_high, tween_duration_glide)
 		else:
-			rhythm_tween.tween_property(rhythm_player, "volume_db", volume_db_low, tween_duration)
+			rhythm_tween.tween_property(rhythm_player, "volume_db", volume_db_low, tween_duration_glide)
+			rhythm_tween.tween_property(rhythm_player, "volume_db", volume_db_zero, tween_duration_cutoff)
