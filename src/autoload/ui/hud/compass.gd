8a27b623e806399c849c2854a7a9186e9d50ef97
Martin Gulliksson <martin@gullik.cc>
2021-07-22 01:07:22 +0200

    ui: add a simple compass

---
+++ src/autoload/ui/hud/compass.gd
@@ -0,0 +1,23 @@
+extends Label
+
+@export var characters: String = "N  |  |  |  |  |  E  |  |  |  |  |  S  |  |  |  |  |  W  |  |  |  |  |  "
+@export var n_characters_visible: int = 16
+
+
+func _process(_delta: float) -> void:
+	var camera: Camera3D = get_viewport().get_camera()
+	if camera == null:
+		return
+
+	var camera_vector: Vector3 = -camera.global_transform.basis.z
+	camera_vector.y = 0.0
+	camera_vector = camera_vector.normalized()
+
+	var camera_angle: float = camera_vector.signed_angle_to(Cardinal.north, Vector3.UP)
+	if camera_angle < 0.0:
+		camera_angle += 2.0 * PI
+
+	var i_char_middle: int = range_lerp(camera_angle, 0.0, 2.0 * PI, 0.0, len(characters))
+	text = ""
+	for i in range(i_char_middle - n_characters_visible, i_char_middle + n_characters_visible):
+		text += characters.substr((i % characters.length() + characters.length()) % characters.length(), 1)
---
+++ src/constants/cardinal.gd
@@ -0,0 +1,7 @@
+class_name Cardinal
+extends Node
+
+const north: Vector3 = Vector3.FORWARD
+const south: Vector3 = -north
+const east: Vector3 = Vector3.RIGHT
+const west: Vector3 = -east
