e13a9feadb49f1ac8b87fb79f5e45663b41bc8b2
Martin Gulliksson <martin@gullik.cc>
2021-07-22 19:26:00 +0200

    git-ipfs: increase hash size

--- src/tools/git-ipfs/git-ipfs.sh
+++ src/tools/git-ipfs/git-ipfs.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 case $1 in
-    clean)  ipfs add --quieter;;
+    clean)  ipfs add --hash=sha2-512 --quieter;;
     smudge) ipfs cat;;
     *)      echo "usage: $0 [clean|smudge]"; exit 1;;
 esac
