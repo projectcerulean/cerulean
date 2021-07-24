7c464fdf518cc5e9f2bd476ef818feec9ecf1f9a
Martin Gulliksson <martin@gullik.cc>
2021-07-24 17:09:38 +0200

    signals: refactor

--- src/autoload/signals/signals.gd
+++ src/autoload/signals/signals.gd
@@ -15,53 +15,53 @@ signal visualize_line
 signal visualize_vector
 
 
-func emit_area_area_entered(sender: Area3D, area: Area3D) -> void:
-	call_deferred("emit_signal", area_area_entered.get_name(), sender, area)
+# Assert that all the signals above have a corresponding emission function below
+func _ready():
+	var node: Node = Node.new()
+	var default_signals: Array[String]
+	var default_methods: Array[String]
+	for s in node.get_signal_list():
+		default_signals.append(s["name"])
+	for m in node.get_method_list():
+		default_methods.append(m["name"])
+	node.queue_free()
+
+	var signals: Array[String]
+	var methods: Array[String]
+	for s in get_signal_list():
+		signals.append(s["name"])
+	for m in get_method_list():
+		methods.append(m["name"])
+
+	for signal_name in signals:
+		if signal_name not in default_signals:
+			assert(("emit_" + signal_name) in methods, "Signal without emission function")
+
+	for method_name in methods:
+		if method_name not in default_methods and method_name.begins_with("emit_"):
+			assert((method_name.trim_prefix("emit_")) in signals, "Emission function without signal")
 
 
-func emit_area_area_exited(sender: Area3D, area: Area3D) -> void:
-	call_deferred("emit_signal", area_area_exited.get_name(), sender, area)
+# Helper function for emitting signals
+func emit(args: Array) -> void:
+	assert(args[0] as Node != null, "Signal sender not a node")
+	var calling_function_name: String = get_stack()[1][&"function"]
+	assert(calling_function_name.begins_with("emit_"), "Emission helper called from unexpected function")
+	var signal_name: StringName = calling_function_name.trim_prefix("emit_")
+	callv(&"call_deferred", [&"emit_signal", signal_name] + args)
 
 
-func emit_area_body_entered(sender: Area3D, body: PhysicsBody3D) -> void:
-	call_deferred("emit_signal", area_body_entered.get_name(), sender, body)
-
-
-func emit_area_body_exited(sender: Area3D, body: PhysicsBody3D) -> void:
-	call_deferred("emit_signal", area_body_exited.get_name(), sender, body)
-
-
-func emit_camera_water_entered(sender: Camera3D) -> void:
-	call_deferred("emit_signal", camera_water_entered.get_name(), sender)
-
-
-func emit_camera_water_exited(sender: Camera3D) -> void:
-	call_deferred("emit_signal", camera_water_exited.get_name(), sender)
-
-
-func emit_debug_write(sender: Node, variant: Variant) -> void:
-	call_deferred("emit_signal", debug_write.get_name(), sender, variant)
-
-
-func emit_request_game_pause(sender: Node) -> void:
-	call_deferred("emit_signal", request_game_pause.get_name(), sender)
-
-
-func emit_request_game_unpause(sender: Node) -> void:
-	call_deferred("emit_signal", request_game_unpause.get_name(), sender)
-
-
-func emit_state_entered(sender: Node, state: Node) -> void:
-	call_deferred("emit_signal", state_entered.get_name(), sender, state)
-
-
-func emit_state_exited(sender: Node, state: Node) -> void:
-	call_deferred("emit_signal", state_exited.get_name(), sender, state)
-
-
-func emit_visualize_line(sender: Node, point: Vector3) -> void:
-	call_deferred("emit_signal", visualize_line.get_name(), sender, point)
-
-
-func emit_visualize_vector(sender: Node, vector: Vector2) -> void:
-	call_deferred("emit_signal", visualize_vector.get_name(), sender, vector)
+# Public signal emission functions
+func emit_area_area_entered(sender: Area3D, area: Area3D) -> void: emit([sender, area])
+func emit_area_area_exited(sender: Area3D, area: Area3D) -> void: emit([sender, area])
+func emit_area_body_entered(sender: Area3D, body: PhysicsBody3D) -> void: emit([sender, body])
+func emit_area_body_exited(sender: Area3D, body: PhysicsBody3D) -> void: emit([sender, body])
+func emit_camera_water_entered(sender: Camera3D) -> void: emit([sender])
+func emit_camera_water_exited(sender: Camera3D) -> void: emit([sender])
+func emit_debug_write(sender: Node, variant: Variant) -> void: emit([sender, variant])
+func emit_request_game_pause(sender: Node) -> void: emit([sender])
+func emit_request_game_unpause(sender: Node) -> void: emit([sender])
+func emit_state_entered(sender: Node, state: Node) -> void: emit([sender, state])
+func emit_state_exited(sender: Node, state: Node) -> void: emit([sender, state])
+func emit_visualize_line(sender: Node, point: Vector3) -> void: emit([sender, point])
+func emit_visualize_vector(sender: Node, vector: Vector2) -> void: emit([sender, vector])
