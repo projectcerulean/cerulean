9ffe13a1bdd6ccaefa56dab301211ddb5081dc1c
Martin Gulliksson <martin@gullik.cc>
2021-07-25 02:41:30 +0200

    debug_console: simplify

--- src/autoload/ui/debug/debug_console/debug_console.gd
+++ src/autoload/ui/debug/debug_console/debug_console.gd
@@ -1,14 +1,12 @@
-extends ColorRect
+extends RichTextLabel
 
 @export var buffer_size_lines: int = 25
 
-@onready var richTextLabel: RichTextLabel = get_node("RichTextLabel")
 @onready var rotationQueue: DataStructures.RotationQueue = DataStructures.RotationQueue.new(buffer_size_lines)
 
 
 func _ready() -> void:
 	SignalsGetter.get_signals().debug_write.connect(self._on_debug_write)
-	assert(richTextLabel != null, Errors.NULL_NODE)
 	assert(rotationQueue != null, Errors.NULL_NODE)
 
 
@@ -20,5 +18,5 @@ func _on_debug_write(sender: Node, variant: Variant) -> void:
 	lines.resize(rotationQueue.size())
 	for iLine in range(rotationQueue.size()):
 		lines[iLine] = rotationQueue.get_item(iLine)
-	richTextLabel.bbcode_text = "\n".join(lines)
+	bbcode_text = "\n".join(lines)
 	visible = true
