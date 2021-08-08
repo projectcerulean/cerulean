2da6e60069c05cb5083079da5741168837110fa6
Martin Gulliksson <martin@gullik.cc>
2021-08-09 00:27:59 +0200

    ui: scale interaction marker depending on camera distance

--- src/autoload/ui/hud/interaction_marker.gd
+++ src/autoload/ui/hud/interaction_marker.gd
@@ -2,10 +2,10 @@ extends Control
 
 @export var color: Color = Color.WHITE
 @export var lfo_resource: Resource
-@export var oscillation_amplitude: float = 16.0
+@export var oscillation_amplitude: float = 128.0
 @export var polygon_shape: PackedVector2Array = PackedVector2Array([
-	Vector2(-25.0, -25.0),
-	Vector2(25.0, -25.0),
+	Vector2(-128.0, -128.0),
+	Vector2(128.0, -128.0),
 	Vector2(0.0, 0.0),
 ])
 
@@ -30,9 +30,11 @@ func _draw() -> void:
 	var position3d: Vector3 = target.global_transform.origin
 	if not camera.is_position_behind(position3d):
 		var position2d = camera.unproject_position(position3d)
+		var camera_distance = (camera.global_transform.origin - position3d).length()
 		var vertex_positions: PackedVector2Array = PackedVector2Array(polygon_shape)
 		for i in range(vertex_positions.size()):
-			vertex_positions[i] += position2d - Vector2(0.0, oscillation_amplitude * abs(lfo_resource.valueFourthShifted))
+			vertex_positions[i] /= camera_distance
+			vertex_positions[i] += position2d - Vector2(0.0, oscillation_amplitude * abs(lfo_resource.valueFourthShifted) / camera_distance)
 		draw_polygon(vertex_positions, color_array)
 
 
