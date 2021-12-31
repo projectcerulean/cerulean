914b840a9ae77ac363ee4f01219e8b9e985692db
Martin Gulliksson <martin@gullik.cc>
2021-12-31 16:19:39 +0100

    interaction_marker: animate when interacting

--- src/autoload/ui/hud/interaction_marker.gd
+++ src/autoload/ui/hud/interaction_marker.gd
@@ -4,19 +4,24 @@ extends Control
 @export var lfo_resource: Resource
 @export var game_state_resource: Resource
 @export var oscillation_amplitude: float = 128.0
+@export var scale_factor_min: float = 0.25
+@export var tween_time: float = 1.0
 @export var polygon_shape: PackedVector2Array = PackedVector2Array([
 	Vector2(-128.0, -128.0),
 	Vector2(128.0, -128.0),
 	Vector2(0.0, 0.0),
 ])
 
+var scale_factor: float = 1.0
 var target: Node3D = null
 
-@onready var color_array: PackedColorArray = PackedColorArray([color, color, color])
+@onready var color_default: Color = color
+@onready var tween: Tween = create_tween()
 
 
 func _ready() -> void:
 	Signals.interaction_highlight_set.connect(self._on_interaction_highlight_set)
+	Signals.request_interaction.connect(self._on_request_interaction)
 	assert(lfo_resource as LfoResource != null, Errors.NULL_RESOURCE)
 	assert(game_state_resource as StateResource != null, Errors.NULL_RESOURCE)
 
@@ -38,9 +43,10 @@ func _draw() -> void:
 		var camera_distance = (camera.global_transform.origin - position3d).length()
 		var vertex_positions: PackedVector2Array = PackedVector2Array(polygon_shape)
 		for i in range(vertex_positions.size()):
+			vertex_positions[i] *= scale_factor
 			vertex_positions[i] /= camera_distance
 			vertex_positions[i] += position2d - Vector2(0.0, oscillation_amplitude * abs(lfo_resource.valueFourthShifted) / camera_distance)
-		draw_polygon(vertex_positions, color_array)
+		draw_polygon(vertex_positions, PackedColorArray([color, color, color]))
 
 
 func _process(_delta: float) -> void:
@@ -49,3 +55,19 @@ func _process(_delta: float) -> void:
 
 func _on_interaction_highlight_set(sender: Node, target: Node3D):
 	self.target = target
+
+
+func _on_request_interaction(sender: Node):
+	if target == null:
+		return
+	
+	scale_factor = scale_factor_min
+	color = Color.WHITE
+	tween.kill()
+	tween = create_tween()
+	tween.set_trans(Tween.TRANS_ELASTIC)
+	tween.set_ease(Tween.EASE_OUT)
+	tween.tween_property(self, "scale_factor", 1.0, tween_time)
+	tween.parallel().set_trans(Tween.TRANS_QUINT)
+	tween.parallel().set_ease(Tween.EASE_OUT)
+	tween.parallel().tween_property(self, "color", color_default, tween_time)
