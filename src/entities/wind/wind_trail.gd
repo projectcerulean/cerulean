5411e065d9d9bc02aae414c79f9e385d93733825
Martin Gulliksson <martin@gullik.cc>
2022-02-04 00:26:16 +0100

    wind_trail: add some curvature

--- src/entities/wind/wind_trail.gd
+++ src/entities/wind/wind_trail.gd
@@ -3,6 +3,9 @@ extends Node3D
 @export var speed: float = 1.0
 @export var lifetime: float = 4.0
 
+@export var noise_texture: NoiseTexture
+@export var noise_strength: float = 1.0
+
 var time: float = 0
 
 @onready var position_horizontal_a: Node3D = get_node("PositionHorizontalA") as Node3D
@@ -11,6 +14,8 @@ var time: float = 0
 @onready var position_vertical_b: Node3D = get_node("PositionVerticalB") as Node3D
 @onready var trail_horizontal: Trail = get_node("Trails/TrailHorizontal") as Trail
 @onready var trail_vertical: Trail = get_node("Trails/TrailVertical") as Trail
+@onready var noise_texture_data: PackedByteArray = noise_texture.get_image().get_data()
+@onready var i_noise_sample: int = randi() % noise_texture_data.size()
 
 
 func _ready() -> void:
@@ -28,4 +33,5 @@ func _process(delta: float) -> void:
 		trail_horizontal.add_segment(position_horizontal_a.global_transform.origin, position_horizontal_b.global_transform.origin)
 		trail_vertical.add_segment(position_vertical_a.global_transform.origin, position_vertical_b.global_transform.origin)
 
-	translate(speed * Vector3.FORWARD)
+	translate(speed * Vector3.FORWARD + (float(noise_texture_data[i_noise_sample]) / 255.0 - 0.5) * noise_strength * Vector3.UP)
+	i_noise_sample = (i_noise_sample + 1) % noise_texture_data.size()
