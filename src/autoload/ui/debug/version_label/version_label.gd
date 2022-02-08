d582777a4af5e838df370263504a253ad13a627e
Martin Gulliksson <martin@gullik.cc>
2022-02-08 20:55:51 +0100

    build: set git hash

--- src/autoload/ui/debug/version_label/version_label.gd
+++ src/autoload/ui/debug/version_label/version_label.gd
@@ -1,38 +1,35 @@
 extends Label
 
 @export var git_folder: String = "res://.git/"
-@export var n_hash_characters_to_show: int = 8
+@export var n_git_hash_characters_to_show: int = 8
 
-const hash_length: int = 40
+const git_hash_length: int = 40
 
 
 func _ready() -> void:
-	# Check for git commit hash
-	var hash: String = ""
-	var file: File = File.new()
-	if file.file_exists(git_folder + "HEAD"):
-		file.open(git_folder + "HEAD", File.READ)
-		var head: String = file.get_as_text().strip_edges()
-		file.close()
-		if head.begins_with("ref: "):
-			if file.file_exists(git_folder + head.trim_prefix("ref: ")):
-				file.open(git_folder + head.trim_prefix("ref: "), File.READ)
-				head = file.get_as_text().strip_edges()
-				file.close()
-				if head.length() == hash_length and head.is_valid_hex_number():
-					hash = head
-		elif head.length() == hash_length and head.is_valid_hex_number():
-			hash = head
-
-	# Show version
-	var version_string: String = (
+	text = (
 		Version.NAME + " "
 		+ str(Version.MAJOR) + "."
 		+ str(Version.MINOR) + "."
 		+ str(Version.PATCH) + "-"
 		+ Version.STATUS
 	)
-	if not hash.is_empty():
-		version_string += "." + hash.substr(0, n_hash_characters_to_show)
 
-	text = version_string
+	var git_hash: String = Version.GIT_HASH
+	if git_hash.is_empty():
+		var file: File = File.new()
+		if file.file_exists(git_folder + "HEAD"):
+			file.open(git_folder + "HEAD", File.READ)
+			var head: String = file.get_as_text().strip_edges()
+			file.close()
+			if head.begins_with("ref: "):
+				if file.file_exists(git_folder + head.trim_prefix("ref: ")):
+					file.open(git_folder + head.trim_prefix("ref: "), File.READ)
+					head = file.get_as_text().strip_edges()
+					file.close()
+					if head.length() == git_hash_length and head.is_valid_hex_number():
+						git_hash = head
+			elif head.length() == git_hash_length and head.is_valid_hex_number():
+				git_hash = head
+	if not git_hash.is_empty():
+		text += "." + git_hash.substr(0, n_git_hash_characters_to_show)
--- src/constants/version.gd
+++ src/constants/version.gd
@@ -7,5 +7,6 @@ const MAJOR: int = 0
 const MINOR: int = 1
 const PATCH: int = 0
 const STATUS: String = "dev"
+const GIT_HASH: String = ""
 const YEAR: int = 2022
 const WEBSITE: String = "https://github.com/projectcerulean/cerulean"
