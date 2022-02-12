a07a244d7cd0a15cb78953519e6c583c8446ca68
Martin Gulliksson <martin@gullik.cc>
2022-02-12 17:12:40 +0100

    data_structures: delete

--- src/autoload/ui/debug/debug_console/debug_console.gd
+++ src/autoload/ui/debug/debug_console/debug_console.gd
@@ -1,22 +1,17 @@
 extends RichTextLabel
 
-@export var buffer_size_lines: int = 25
+@export var lines_to_show: int = 25
 
-@onready var rotation_queue: DataStructures.RotationQueue = DataStructures.RotationQueue.new(buffer_size_lines)
+var buffer: PackedStringArray = []
 
 
 func _ready() -> void:
-	Signals.debug_write.connect(self._on_debug_write)
-	assert(rotation_queue != null, Errors.NULL_NODE)
+	Signals.debug_write.connect(_on_debug_write)
 
 
 func _on_debug_write(sender: Node, variant: Variant) -> void:
 	var string: String = str(variant)
 	var hex_color: String = CColor.str_to_color(sender.name).to_html(false)
-	rotation_queue.add("[color=#%s][code][%s]:[/code][/color] %s" % [hex_color, sender.name, string])
-	var lines: Array[String] = []
-	lines.resize(rotation_queue.size())
-	for i_line in range(rotation_queue.size()):
-		lines[i_line] = rotation_queue.get_item(i_line)
-	text = "\n".join(lines)
+	buffer.append("[color=#%s][code][%s]:[/code][/color] %s" % [hex_color, sender.name, string])
+	text = "\n".join(buffer.slice(buffer.size() - lines_to_show))
 	visible = true
