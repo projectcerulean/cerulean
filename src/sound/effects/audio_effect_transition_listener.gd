7a7be7817b6188a1b78f19f58a88616789a9bdd0
Martin Gulliksson <martin@gullik.cc>
2022-02-16 07:43:44 +0100

    audio_effect_transition_listener: add tweening

--- src/sound/effects/audio_effect_transition_listener.gd
+++ src/sound/effects/audio_effect_transition_listener.gd
@@ -1,11 +1,21 @@
 class_name AudioEffectTransitionListener
 extends StateTransitionListener
 
+const wet_level_min: float = 0.0
+const wet_level_max: float = 1.0
+
+const TWEEN_TYPE_EXPONENTIAL: int = 0
+const TWEEN_TYPE_LINEAR: int = 1
+
 @export var audio_effect_dry: AudioEffect
 @export var audio_effect_wet: AudioEffect
 @export var audio_bus_name: StringName = AudioBuses.EFFECTS
+@export var tween_duration: float = 1.0
+@export_enum(Exponential, Linear) var tween_type: int = TWEEN_TYPE_EXPONENTIAL
 
 var audio_effect: AudioEffect
+var tween: Tween
+var wet_level: float
 
 @onready var bus_index: int = AudioServer.get_bus_index(audio_bus_name)
 
@@ -16,6 +26,7 @@ func _ready() -> void:
 	assert(audio_effect_wet != null, Errors.NULL_RESOURCE)
 	assert(audio_effect_dry.get_class() == audio_effect_wet.get_class(), Errors.TYPE_ERROR)
 	assert(bus_index >= 0, Errors.INVALID_AUDIO_BUS)
+	assert(tween_duration >= 0.0, Errors.INVALID_ARGUMENT)
 
 	audio_effect = audio_effect_dry.duplicate()
 
@@ -37,13 +48,28 @@ func _notification(what: int) -> void:
 
 func _on_target_state_entered() -> void:
 	super._on_target_state_entered()
-	for property in audio_effect.get_property_list():
-		if property.type == TYPE_FLOAT:
-			audio_effect.set(property.name, audio_effect_wet.get(property.name))
+	if tween != null:
+		tween.kill()
+	tween = create_tween()
+	tween.tween_method(set_wet_level, wet_level, wet_level_max, tween_duration)
 
 
 func _on_target_state_exited() -> void:
 	super._on_target_state_exited()
+	if tween != null:
+		tween.kill()
+	tween = create_tween()
+	tween.tween_method(set_wet_level, wet_level, wet_level_min, tween_duration)
+
+
+func set_wet_level(wet_level_new: float):
+	assert(wet_level_new >= wet_level_min and wet_level_new <= wet_level_max, Errors.INVALID_ARGUMENT)
+	wet_level = wet_level_new
 	for property in audio_effect.get_property_list():
 		if property.type == TYPE_FLOAT:
-			audio_effect.set(property.name, audio_effect_dry.get(property.name))
+			var value: float = 0.0
+			if tween_type == TWEEN_TYPE_EXPONENTIAL:
+				value = exp(lerp(log(audio_effect_dry.get(property.name)), log(audio_effect_wet.get(property.name)), wet_level))
+			elif tween_type == TWEEN_TYPE_LINEAR:
+				value = lerp(audio_effect_dry.get(property.name), audio_effect_wet.get(property.name), wet_level)
+			audio_effect.set(property.name, value)
