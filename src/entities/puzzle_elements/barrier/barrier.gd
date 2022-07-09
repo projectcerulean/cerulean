47ea8aac9509901bc4403ebef49a48eaf8fd5d18
Martin Gulliksson <martin@gullik.cc>
2022-07-09 22:58:03 +0200

    barrier: improve input node error checking

--- src/entities/puzzle_elements/barrier/barrier.gd
+++ src/entities/puzzle_elements/barrier/barrier.gd
@@ -4,10 +4,11 @@
 class_name Barrier
 extends StaticBody3D
 
-@export var input_nodes: Array[NodePath]
+@export var _input_nodes: Array[NodePath]
 @export var input_targets: Array[bool]
 @export var fade_duration: float = 0.25
 
+var input_nodes: Array[Node]
 var inputs: Array[bool]
 
 @onready var collision_shape: CollisionShape3D = get_node("CollisionShape3D") as CollisionShape3D
@@ -19,19 +20,28 @@ var inputs: Array[bool]
 
 func _ready() -> void:
 	Signals.state_entered.connect(self._on_state_entered)
-	assert(len(input_nodes) == len(input_targets), Errors.INVALID_ARGUMENT)
+	assert(len(_input_nodes) == len(input_targets), Errors.INVALID_ARGUMENT)
 	assert(collision_shape != null, Errors.NULL_NODE)
 	assert(mesh_instance != null, Errors.NULL_NODE)
 	assert(state_machine != null, Errors.NULL_NODE)
 	assert(shader_material != null, Errors.NULL_RESOURCE)
 
+	input_nodes.resize(len(input_targets))
 	inputs.resize(len(input_targets))
+
+	for i in range(len(_input_nodes)):
+		input_nodes[i] = get_node(_input_nodes[i])
+		assert(input_nodes[i] != null, Errors.NULL_NODE)
+		for j in range(len(_input_nodes)):
+			if i == j:
+				continue
+			assert(input_nodes[i] != input_nodes[j], Errors.INVALID_ARGUMENT)
 
 
 func _on_state_entered(sender: Node, state: StringName, _data: Dictionary) -> void:
 	var inputs_changed: bool = false
 	for i in range(len(inputs)):
-		if sender.owner == get_node(input_nodes[i]):
+		if sender.owner == input_nodes[i]:
 			assert(state in [PuzzleElementStates.DISABLED, PuzzleElementStates.ENABLED], Errors.CONSISTENCY_ERROR)
 			inputs[i] = state == PuzzleElementStates.ENABLED
 			inputs_changed = true
