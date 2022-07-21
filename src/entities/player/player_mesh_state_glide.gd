c715c97626946e1f02a9b2a03b481c616bfe5e8a
Martin Gulliksson <martin@gullik.cc>
2022-07-22 00:04:20 +0200

    player: spin when bouncing

--- src/entities/player/player_mesh_state_glide.gd
+++ src/entities/player/player_mesh_state_glide.gd
@@ -5,30 +5,36 @@ extends PlayerMeshState
 
 @export var turn_lerp_weight: float = 0.1
 @export var roll_lerp_weight: float = 2.0
+@export var spin_rate: float = 0.0
 
 var input_direction_planar: Vector3
 var yaw_direction: Vector3 = Vector3.ZERO
 var roll_angle: float = 0.0
+var roll_angle_spin: float = 0.0
+var spin_direction: float = 0.0
 
 
 func enter(data: Dictionary) -> void:
 	super.enter(data)
 	roll_angle = data.get(ROLL_ANGLE, 0.0)
+	roll_angle_spin = 0.0
 	yaw_direction = data.get(YAW_DIRECTION, Vector3.ZERO)
 	input_direction_planar = yaw_direction
 	mesh_root.look_at(mesh_root.global_position + player.linear_velocity.normalized())
+	spin_direction = 1.0 if randf() > 0.5 else -1.0
 	process(get_process_delta_time())
 
 
 func exit(data: Dictionary) -> void:
 	super.exit(data)
-	data[ROLL_ANGLE] = roll_angle
+	data[ROLL_ANGLE] = roll_angle + roll_angle_spin * spin_direction
 	data[YAW_DIRECTION] = yaw_direction
 	data[YAW_DIRECTION_TARGET] = yaw_direction
 
 
 func process(delta: float) -> void:
 	super.process(delta)
+	roll_angle_spin += player.linear_velocity.y * spin_rate * delta
 	var velocity_planar: Vector3 = Vector3(player.linear_velocity.x, 0.0, player.linear_velocity.z)
 
 	var yaw_direction_new: Vector3 = velocity_planar.normalized()
@@ -51,4 +57,4 @@ func process(delta: float) -> void:
 		input_vector_spherical = input_vector_spherical.normalized()
 		assert(input_vector_spherical.is_normalized(), Errors.CONSISTENCY_ERROR)
 		var mesh_direction: Vector3 = player.linear_velocity.slerp(input_vector_spherical, turn_lerp_weight).normalized()
-		mesh_root.look_at(mesh_root.global_position + mesh_direction, Vector3.UP.rotated(mesh_direction, -roll_angle))
+		mesh_root.look_at(mesh_root.global_position + mesh_direction, Vector3.UP.rotated(mesh_direction, -(roll_angle + roll_angle_spin * spin_direction)))
