245399461fa19c396c4489a0120985d9e305e773
Martin Gulliksson <martin@gullik.cc>
2022-10-15 15:42:19 +0200

    add missing license identifiers

--- src/entities/rotator/rotator.gd
+++ src/entities/rotator/rotator.gd
@@ -1,3 +1,6 @@
+# This file is part of Project Cerulean <https://projectcerulean.org>
+# Copyright (C) 2021-2022 Martin Gulliksson
+# SPDX-License-Identifier: GPL-3.0-or-later
 extends AnimatableBody3D
 
 @export var angular_velocity: float = 0.5
