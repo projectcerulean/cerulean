965193e49e86901f97e03d534c9b9bc06b1fafd3
Martin Gulliksson <martin@gullik.cc>
2023-07-16 20:18:33 +0200

    math: add unit test

--- src/common/math/math.gd
+++ src/common/math/math.gd
@@ -7,7 +7,3 @@ extends Node
 
 static func signed_sqrt(x: float) -> float:
 	return sign(x) * sqrt(abs(x))
-
-
-static func ellipse(radii: Vector2, theta: float):
-	return radii.x * radii.y / sqrt((radii.y * cos(theta)) * (radii.y * cos(theta)) + (radii.x * sin(theta)) * (radii.x * sin(theta)))
---
+++ src/test/unit/common/math/test_math.gd
@@ -0,0 +1,18 @@
+# This file is part of Project Cerulean <https://projectcerulean.org>
+# Copyright (C) 2021-2023 Martin Gulliksson
+# SPDX-License-Identifier: GPL-3.0-or-later
+extends GutTest
+
+const ERROR_INTERVAL: float = 0.001
+
+
+func test_signed_sqrt_positive() -> void:
+	assert_almost_eq(Math.signed_sqrt(4.0), 2.0, ERROR_INTERVAL)
+
+
+func test_signed_sqrt_negative() -> void:
+	assert_almost_eq(Math.signed_sqrt(-4.0), -2.0, ERROR_INTERVAL)
+
+
+func test_signed_sqrt_zero() -> void:
+	assert_almost_eq(Math.signed_sqrt(0.0), 0.0, ERROR_INTERVAL)
