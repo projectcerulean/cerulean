c1a183aa4e6d82a3fcd37fa4ad5d2bf9f127b966
Martin Gulliksson <martin@gullik.cc>
2023-08-02 22:44:21 +0200

    persistent_data: make sure resource is never freed

---
+++ src/singletons/persistent_data/persistent_data_manager.gd
@@ -0,0 +1,11 @@
+# This file is part of Project Cerulean <https://projectcerulean.org>
+# Copyright (C) 2021-2023 Martin Gulliksson
+# SPDX-License-Identifier: GPL-3.0-or-later
+extends Node
+## Keeps a reference to the persistent data resource to make sure it is never freed.
+
+@export var persistent_data_resource: DictionaryResource
+
+
+func _ready() -> void:
+	assert(persistent_data_resource != null, Errors.NULL_RESOURCE)
