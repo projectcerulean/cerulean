6210b4691929e813adfd2e575ef402fbe14ad17b
Martin Gulliksson <martin@gullik.cc>
2023-08-17 17:49:17 +0200

    scene_manager: properly handle godot's 'run current scene' option

--- src/entities/scene/scene.gd
+++ src/entities/scene/scene.gd
@@ -6,4 +6,20 @@ extends Node
 
 
 func _ready() -> void:
+	if is_same(self, get_tree().current_scene):
+		# Handle Godot's 'Run Current Scene' option
+		_restart_scene_with_proper_scene_tree_root()
+		return
+
 	Signals.emit_scene_changed(self)
+
+
+func _restart_scene_with_proper_scene_tree_root() -> void:
+	var main_scene: PackedScene = load("res://src/main/main.tscn") as PackedScene
+	assert(main_scene != null, Errors.NULL_RESOURCE)
+	var main_scene_instance: Main = main_scene.instantiate() as Main
+	assert(main_scene_instance != null, Errors.NULL_NODE)
+	main_scene_instance.scene_default = scene_file_path
+	var main_scene_updated: PackedScene = PackedScene.new()
+	main_scene_updated.pack(main_scene_instance)
+	get_tree().change_scene_to_packed(main_scene_updated)
--- src/environment/effects/underwater_fog.gd
+++ src/environment/effects/underwater_fog.gd
@@ -20,16 +20,23 @@ func _notification(what: int) -> void:
 
 
 func _on_water_entered(sender: NodePath) -> void:
-	var sender_node: Node = get_node(sender)
-	if is_instance_valid(sender_node):
-		var sender_node_owner: Node = sender_node.owner
-		if is_instance_valid(sender_node_owner) and sender_node_owner == get_viewport().get_camera_3d().owner:
-			environment_resource.value.fog_enabled = true
+	handle_water_entered_exited_signal(sender, true)
 
 
 func _on_water_exited(sender: NodePath) -> void:
+	handle_water_entered_exited_signal(sender, false)
+
+
+func handle_water_entered_exited_signal(sender: NodePath, is_water_entered: bool) -> void:
 	var sender_node: Node = get_node(sender)
 	if is_instance_valid(sender_node):
 		var sender_node_owner: Node = sender_node.owner
-		if is_instance_valid(sender_node_owner) and sender_node_owner == get_viewport().get_camera_3d().owner:
-			environment_resource.value.fog_enabled = false
+		if is_instance_valid(sender_node_owner):
+			var viewport: Viewport = get_viewport()
+			if is_instance_valid(viewport):
+				var camera: Camera3D = viewport.get_camera_3d()
+				if is_instance_valid(camera):
+					var camera_owner: Node = camera.owner
+					if is_instance_valid(camera_owner):
+						if is_same(sender_node_owner, camera_owner):
+							environment_resource.value.fog_enabled = is_water_entered
