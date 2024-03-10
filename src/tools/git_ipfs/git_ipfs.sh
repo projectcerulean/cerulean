4409f70f8208cf8d5bc1b63f2f3fe5ce6ab1cfa1
Martin Gulliksson <martin@gullik.cc>
2024-03-10 12:25:08 +0100

    build: use debian image instead of arch, for better build reproducibility

--- src/tools/git_ipfs/git_ipfs.sh
+++ src/tools/git_ipfs/git_ipfs.sh
@@ -3,8 +3,10 @@
 # Copyright (C) 2021-2024 Martin Gulliksson
 # SPDX-License-Identifier: GPL-3.0-or-later
 
+IPFS="${IPFS:-ipfs}"
+
 case $1 in
-    clean)  ipfs add --hash=sha2-512 --chunker=size-262144 --quieter;;
-    smudge) ipfs cat;;
+    clean)  "${IPFS}" add --hash=sha2-512 --chunker=size-262144 --quieter;;
+    smudge) "${IPFS}" cat;;
     *)      echo "usage: $0 [clean|smudge]"; exit 1;;
 esac
