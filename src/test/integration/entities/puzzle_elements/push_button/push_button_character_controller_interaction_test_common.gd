0817aa26ad8ce2a0d6633c3d37eef0717825b060
Martin Gulliksson <martin@gullik.cc>
2024-04-18 20:23:03 +0200

    push_button: test that button is deactivated when stepping off of it

--- src/test/integration/entities/puzzle_elements/push_button/push_button_character_controller_interaction_test_common.gd
+++ src/test/integration/entities/puzzle_elements/push_button/push_button_character_controller_interaction_test_common.gd
@@ -4,31 +4,28 @@
 class_name PushButtonCharacterControllerInteractionTestCommon
 extends IntegrationTest
 
-const WAIT_PROCESS_FRAMES: int = 5
-const COLLISION_TIMEOUT_PHYSICS_FRAMES: int = 60
+const INITIAL_WAIT_PROCESS_FRAMES: int = 3
+const MAX_WAIT_PHYSICS_FRAMES: int = 60
 
 
 func test_push_button_character_controller_interaction() -> void:
 	var test_scene: PushButtonCharacterControllerInteraction =  get_test_scene() as PushButtonCharacterControllerInteraction
 	assert_false(test_scene.barrier.collision_shape.disabled, "Push button immediately pressed after instantiation")
 
-	await wait_for_process_frames(WAIT_PROCESS_FRAMES)
+	await wait_for_process_frames(INITIAL_WAIT_PROCESS_FRAMES)
 	assert_false(test_scene.barrier.collision_shape.disabled, "Push button already pressed a few frames after instantiation")
 
 	# Wait for the character controller to fall onto the button due to gravity
-	var character_controller_y_prev: float = NAN
-	var has_collided: bool = false
-	for i: int in range(COLLISION_TIMEOUT_PHYSICS_FRAMES):
-		if not is_nan(character_controller_y_prev):
-			if (
-				is_equal_approx(test_scene.character_controller.global_position.y, character_controller_y_prev)
-				or test_scene.character_controller.global_position.y > character_controller_y_prev
-			):
-				has_collided = true
-				break
-		character_controller_y_prev = test_scene.character_controller.global_position.y
+	for i: int in range(MAX_WAIT_PHYSICS_FRAMES):
 		await wait_for_physics_frame()
-	assert_true(has_collided, "Character controller not collide with push button")
-
-	await wait_for_process_frames(WAIT_PROCESS_FRAMES)
+		if test_scene.barrier.collision_shape.disabled:
+			break
 	assert_true(test_scene.barrier.collision_shape.disabled, "Push button not pressed after collision with character controller")
+
+	# Push the character controller off of the button
+	test_scene.character_controller.apply_central_impulse(1000.0 * Vector3.FORWARD * test_scene.character_controller.mass)
+	for i: int in range(MAX_WAIT_PHYSICS_FRAMES):
+		await wait_for_physics_frame()
+		if not test_scene.barrier.collision_shape.disabled:
+			break
+	assert_false(test_scene.barrier.collision_shape.disabled, "Push button still pressed after moving character controller off of it")
