da5a71618c0c439f6b03bd47dc45f7b01a8fd25a
Martin Gulliksson <martin@gullik.cc>
2025-01-06 21:43:50 +0100

    box: prevent box from being pushed around if it's on the ground

--- src/entities/box/box.gd
+++ src/entities/box/box.gd
@@ -23,3 +23,19 @@ func _physics_process(_delta: float) -> void:
 			var force_position: Vector3 = water_detector.global_position - global_position
 			apply_central_force(buoyancy_central_factor * force * Vector3.UP)
 			apply_force((1.0 - buoyancy_central_factor) * force * Vector3.UP, force_position)
+
+	# Prevent box from being pushed around if it's on the ground
+	var is_atleast_one_water_detector_in_water: bool = false
+	for i: int in range(water_detectors.get_child_count()):
+		var water_detector: WaterDetector = water_detectors.get_child(i) as WaterDetector
+		if water_detector.is_in_water():
+			is_atleast_one_water_detector_in_water = true
+			break
+	if is_atleast_one_water_detector_in_water:
+		axis_lock_angular_y = false
+		axis_lock_linear_x = false
+		axis_lock_linear_z = false
+	else:
+		axis_lock_angular_y = true
+		axis_lock_linear_x = true
+		axis_lock_linear_z = true
